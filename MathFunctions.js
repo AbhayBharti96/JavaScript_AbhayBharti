b9954e1ccbf0520719dc2888154a547baf092d76
Abhay Bharti <154121097+AbhayBharti96@users.noreply.github.com>
2025-08-16 10:49:19 +0000

    MathFunctions

---
+++ MathFunctions.js
@@ -0,0 +1,15 @@
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));//Top value like it should be choose 4.5
+console.log(Math.floor(4.9));//Choose lower value 4 
+console.log(Math.sqrt(4));
+console.log(Math.pow(4,3));
+console.log(Math.max(4,4,5,7,8,9));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+
+const min =10
+const max = 20
+console.log(Math.floor(Math.random()*(max-min+1))+min)
